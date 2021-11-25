756128372fd7ac5b6de2f0e644a9e246216eaf17
Andy <wanghaoxin5277@gmail.com>
2021-11-25 21:55:00 +0800

    provider

--- src/main/java/com/xia/yuauth/YuAuthApplication.java
+++ src/main/java/com/xia/yuauth/YuAuthApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@ImportResource(value = "classpath:/dubbo/dubbo-consumer.xml")
+@ImportResource(value = {"classpath:/dubbo/consumer.xml", "classpath:/dubbo/provider.xml"})
 @SpringBootApplication
 public class YuAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YuAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YuAuthApplication.class, args);
+    }
 
 }
